a3ebe0f1a4358c97fda282b0effa28e8c303b486
LABCONTROL\421-33 <422055662@pcpuma.acatlan.unam.mx>
2022-08-22 08:22:29 -0500

    carpetas

---
+++ data_structure/tareas/programas/factorial/2do programa 421065033.c
@@ -0,0 +1,11 @@
+//Esta es la librería -estándar- de entrada y salida. Incluye a la función printf y scan
+#include <stdio.h>
+/* la función printf es la encargada de mostrar un texto en pantalla*/
+
+/* "main()"* es la función principal donde se leen las instrucciones que se han codificado*/
+
+main() {
+	printf ("Sergio Alfonso Sanchez Garcia\n");
+	printf("421065033\n");
+	printf("13 de diciembre del 2001	-	13/12/2001\n");
+}
